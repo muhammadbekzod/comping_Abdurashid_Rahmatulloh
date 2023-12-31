73aab0ee0f9c616039eeb2510d102f4938e366ab
Bekzod Abdulkhaev <abdulkhaev96@gmail.com>
2023-12-31 22:03:32 +0900

    Pagenation&getById&Deleted

--- controller/motors.controller.js
+++ controller/motors.controller.js
@@ -1,7 +1,24 @@
 const asyncHandler = require("../middleware/async");
 const Motors = require("../models/motors");
 
-exports.getAllMotors = async (req, res, next) => {};
+exports.getAllMotors = asyncHandler(async (req, res, next) => {
+  const pageLimit = process.env.DEFAULT_PAGE_LIMIT || 10;
+
+  const limit = parseInt(req.query.limit || pageLimit);
+  const page = parseInt(req.query.page || 1);
+  const total = await Motors.countDocuments();
+
+  const motors = await Motors.find()
+    .skip(page * limit - limit)
+    .limit(limit);
+  res.status(200).json({
+    success: true,
+    pageCount: Math.ceil(total / limit),
+    currentPage: page,
+    next: Math.ceil(total / limit) < page + 1 ? null : page + 1,
+    data: motors,
+  });
+});
 
 exports.createNewMotor = asyncHandler(async (req, res, next) => {
   const newMotor = await Motors.create({
@@ -15,4 +32,17 @@ exports.createNewMotor = asyncHandler(async (req, res, next) => {
     success: true,
     data: newMotor,
   });
+});
+
+exports.getByMotorId = asyncHandler(async (req, res, next) => {
+  const motor = await Motors.findById(req.params.id);
+  res.status(200).json({
+    success: true,
+    data: motor,
+  });
+});
+
+exports.deleteMotor = asyncHandler(async (req, res, next) => {
+  await Motors.findByIdAndDelete(req.params.id);
+  res.status(200).send("Deleted Successfully");
 });
--- routes/motor.route.js
+++ routes/motor.route.js
@@ -1,7 +1,15 @@
 const { Router } = require("express");
-const { createNewMotor } = require("../controller/motors.controller");
+const {
+  createNewMotor,
+  getByMotorId,
+  getAllMotors,
+  deleteMotor,
+} = require("../controller/motors.controller");
 
 const router = Router();
 
+router.get("/getAllMotors", getAllMotors);
 router.post("/addNewMotor", createNewMotor);
+router.get("/:id", getByMotorId);
+router.delete("/:id", deleteMotor);
 module.exports = router;
